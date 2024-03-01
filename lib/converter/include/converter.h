9828b164c8f1ac1508b77295b1c52a040793ead3
Niklas Meyer <niklasbhv@gmail.com>
2024-03-01 11:49:08 +0100

    Fixed building

--- lib/converter/include/converter.h
+++ lib/converter/include/converter.h
@@ -7,7 +7,7 @@
 
 #include <nlohmann/json.hpp>
 
-int loadJsonFile(const char* path, nlohmann::json json_file);
+int loadJsonFile(const char* path, nlohmann::json& json_file);
 int convertSdfToMatter(const nlohmann::json& sdf_model, const nlohmann::json& sdf_mapping);
 
 #endif //CONVERTER_H
