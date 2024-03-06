9d7eea728d6ce14eb780680df70fb95db2b1d3e3
Niklas Meyer <niklasbhv@gmail.com>
2024-03-06 08:00:01 +0100

    Expanded structs

--- lib/converter/include/matter.h
+++ lib/converter/include/matter.h
@@ -9,11 +9,25 @@
 #include <map>
 #include <list>
 
+struct accessType{
+    std::string op;
+    std::string role;
+    std::string privilege;
+    std::string modifier;
+};
+
+struct fieldType{
+    std::string mask; // required
+    std::string name; //required
+    std::string introducedIn; //type: zxlSpecVersion
+    int fieldId;
+};
+
 struct bitmapType{
-    std::string name;
-    std::string type;
-    std::string cluster;
-    std::map<std::string, std::string> fields;
+    std::string name; // required
+    std::string type; // required
+    std::string cluster; // min = 0; max = inf; name = cluster
+    std::list<fieldType> fields; // min = 1; max = inf; ref=field
 };
 
 struct enumType{
@@ -24,30 +38,43 @@ struct enumType{
 };
 
 struct eventFieldType{
-    std::string id;
-    std::string name;
-    std::string type;
-    bool array;
-    int length;
+    std::string id; // required
+    std::string name; // required
+    std::string type; // required
+    bool array; //optional
+    bool isNullable; //optional
+    //int length; TODO: Does not seem to be part of the schema
 };
 
 struct eventType {
+    // ref=description
+    std::list<accessType> access; // min = 0; max = inf
+    std::list<eventFieldType> field; // min = 0; max = inf
     std::string code;
     std::string name;
+    std::string side; // required; either server or client
     std::string priority;
-    std::string side;
-    bool optional;
-    std::string description;
-    std::list<eventFieldType> fields;
+    //bool optional; TODO: Does not seem to be part of the schema
 };
 
 struct commandType {
-    std::string source;
+    // ref=description
+    std::list<accessType> access; // min = 0; max = inf
+    //std::list<argType> arg; // min = 0; max = inf
+    //cli
+    std::string cliFunctionName;
     std::string code;
+    bool disableDefaultResponse;
+    std::string functionName;
+    std::string group;
+    std::string introducedIn; //type: zxlSpecVersion
+    bool noDefaultImplementation;
+    std::string manufacturerCode;
     std::string name;
     bool optional;
-    std::string description;
-
+    std::string source;
+    std::string restriction;
+    std::string response;
 };
 
 struct attributeType {
