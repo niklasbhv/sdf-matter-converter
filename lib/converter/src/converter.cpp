a7812b0f3b4b21c2ce2a6d41cf64adaa5a963927
Niklas Meyer <niklasbhv@gmail.com>
2024-03-06 11:03:47 +0100

    Fixed convertToSdf

--- lib/converter/src/converter.cpp
+++ lib/converter/src/converter.cpp
@@ -3,7 +3,6 @@
 //
 #include "matter.h"
 #include "sdf.h"
-//TODO: Currently temporary
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include <pugixml.hpp>
@@ -23,31 +22,22 @@ int loadJsonFile(const char* path, json& json_file)
 int loadXmlFile(const char* path, pugi::xml_document& xml_file)
 {
     pugi::xml_parse_result result = xml_file.load_file(path);
-    std::cout << path << std::endl;
     if (!result){
-        std::cout << "Could not load XML file" << std::endl;
+        std::cerr << "Could not load XML file" << std::endl;
         return -1;
     }
-    std::cout << xml_file.root().name() << std::endl;
     return 0;
 }
 
 //TODO: This only works if we can store multiple cluster definitions inside a single xml file
 int convertMatterToSdf(const pugi::xml_document& device_xml, const pugi::xml_document& cluster_xml)
 {
-    //TODO: We can initialize this as a fixed size array by using the number of dynamic endpoints
-    std::cout << device_xml.value() << std::endl;
-    std::list<deviceType> devices;
-    //! Iterate through all deviceType children
-    for (pugi::xml_node device_type_node: device_xml.children("configurator")) {
-        deviceType device;
-        //mapDevice(device_type_node, device);
-        devices.push_back(device);
-        const std::basic_string<char> device_name = device.name;
-        std::cout << device.name << std::endl;
-    }
-
-    //mapCluster(cluster_xml);
+    std::list<deviceType> deviceList;
+    parseDevices(device_xml, deviceList);
+    std::list<clusterType> clusterList;
+    parseClusters(cluster_xml, clusterList);
+    std::cout << deviceList.size() << std::endl;
+    std::cout << clusterList.size() << std::endl;
     return 0;
 }
 
