3ae4e56a3d75251a9152a7f9f17f19db3ddadf83
Niklas Meyer <niklasbhv@gmail.com>
2024-03-08 13:45:27 +0100

    Add initial charge of functions for mapping

--- lib/converter/include/mapping.h
+++ lib/converter/include/mapping.h
@@ -1,14 +1,10 @@
-//
-// Created by niklas on 08.03.24.
-//
-
 #ifndef SDF_MATTER_CONVERTER_MAPPING_H
 #define SDF_MATTER_CONVERTER_MAPPING_H
 
+#include "matter.h"
+#include "sdf.h"
 
-class mapping {
-
-};
-
+int map_sdf_to_matter(sdfModelType& sdfModel, sdfMappingType& sdfMappingType);
+int return_matter_to_sdf(deviceType& device, clusterType& cluster);
 
 #endif //SDF_MATTER_CONVERTER_MAPPING_H
--- lib/converter/src/mapping.cpp
+++ lib/converter/src/mapping.cpp
@@ -1,13 +1,63 @@
-//
-// Created by niklas on 08.03.24.
-//
-
 #include "mapping.h"
+#include "matter.h"
+#include "sdf.h"
 
-int map_sdf_to_matter(){
+int map_sdf_to_matter(sdfModelType& sdfModel, sdfMappingType& sdfMappingType)
+{
     return 0;
 }
 
-int return_matter_to_sdf(){
+int map_sdf_thing(sdfThingType& sdfThing, deviceType& device)
+{
+    return 0;
+}
+
+int map_sdf_object(sdfObjectType& sdfObject, clusterType& cluster)
+{
+    return 0;
+}
+
+int map_sdf_property(sdfPropertyType& sdfProperty, attributeType& attribute)
+{
+    return 0;
+}
+
+int map_sdf_action(sdfActionType& sdfAction, commandType& command)
+{
+    return 0;
+}
+
+int map_sdf_event(sdfEventType& sdfEvent, eventType& event)
+{
+    return 0;
+}
+
+int return_matter_to_sdf(deviceType& device, clusterType& cluster)
+{
+    return 0;
+}
+
+int map_matter_device()
+{
+    return 0;
+}
+
+int map_matter_cluster()
+{
+    return 0;
+}
+
+int map_matter_attribute()
+{
+    return 0;
+}
+
+int map_matter_command()
+{
+    return 0;
+}
+
+int map_matter_event()
+{
     return 0;
 }
