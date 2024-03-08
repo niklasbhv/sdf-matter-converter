1033117fbc4da76f2113a613d0742b062429e4d4
Niklas Meyer <niklasbhv@gmail.com>
2024-03-08 13:36:40 +0100

    Added mapping source file

---
+++ lib/converter/include/mapping.h
@@ -0,0 +1,14 @@
+//
+// Created by niklas on 08.03.24.
+//
+
+#ifndef SDF_MATTER_CONVERTER_MAPPING_H
+#define SDF_MATTER_CONVERTER_MAPPING_H
+
+
+class mapping {
+
+};
+
+
+#endif //SDF_MATTER_CONVERTER_MAPPING_H
---
+++ lib/converter/src/mapping.cpp
@@ -0,0 +1,13 @@
+//
+// Created by niklas on 08.03.24.
+//
+
+#include "mapping.h"
+
+int map_sdf_to_matter(){
+    return 0;
+}
+
+int return_matter_to_sdf(){
+    return 0;
+}
