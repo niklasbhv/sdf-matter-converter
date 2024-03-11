d47f58fe85834ddc4ec0add6e0e60e035e9ce560
Niklas Meyer <niklasbhv@gmail.com>
2024-03-11 08:06:22 +0100

    Implemented map_matter_device

--- lib/converter/include/mapping.h
+++ lib/converter/include/mapping.h
@@ -1,5 +1,5 @@
-#ifndef SDF_MATTER_CONVERTER_MAPPING_H
-#define SDF_MATTER_CONVERTER_MAPPING_H
+#ifndef MAPPING_H
+#define MAPPING_H
 
 #include "matter.h"
 #include "sdf.h"
@@ -7,4 +7,4 @@
 int map_sdf_to_matter(sdfModelType& sdfModel, sdfMappingType& sdfMappingType);
 int return_matter_to_sdf(deviceType& device, clusterType& cluster);
 
-#endif //SDF_MATTER_CONVERTER_MAPPING_H
+#endif //MAPPING_H
--- lib/converter/src/mapping.cpp
+++ lib/converter/src/mapping.cpp
@@ -32,31 +32,59 @@ int map_sdf_event(sdfEventType& sdfEvent, eventType& event)
     return 0;
 }
 
-int return_matter_to_sdf(deviceType& device, clusterType& cluster)
+//! Matter -> SDF
+int matter_to_sdf(deviceType& device, clusterType& cluster)
 {
     return 0;
 }
 
-int map_matter_device(deviceType& device, sdfModelType& sdfModel)
-{
-    return 0;
-}
-
+//! Matter Cluster -> sdfObject
 int map_matter_cluster(clusterType& cluster, sdfObjectType& sdfObject)
 {
     return 0;
 }
 
+//! Matter Device -> SDF-Model
+int map_matter_device(deviceType& device, sdfModelType& sdfModel)
+{
+    //! Information Block
+    infoBlockType infoBlock;
+    infoBlock.title = device.name;
+    sdfModel.infoBlock = infoBlock;
+
+    //! Namespace Block
+    namespaceType namespaceBlock;
+    sdfModel.namespaceBlock = namespaceBlock;
+
+    //! Definition Block
+    sdfThingType sdfThing;
+    sdfCommonType commonQualities;
+    commonQualities.label = device.name;
+    sdfThing.commonQualities = commonQualities;
+
+    std::map<std::string, sdfObjectType> sdfObjectMap;
+    for (clusterType& cluster : device.clusters){
+        sdfObjectType sdfObject;
+        map_matter_cluster(cluster, sdfObject);
+        sdfObjectMap.insert({cluster.name, sdfObject});
+    }
+    sdfThing.sdfObject = sdfObjectMap;
+    return 0;
+}
+
+//! Matter Attribute -> sdfProperty
 int map_matter_attribute(attributeType& attribute, sdfPropertyType& sdfProperty)
 {
     return 0;
 }
 
+//! Matter Command -> sdfAction
 int map_matter_command(commandType& command, sdfActionType& sdfAction)
 {
     return 0;
 }
 
+//! Matter Event -> sdfEvent
 int map_matter_event(eventType& event, sdfEventType& sdfEvent)
 {
     return 0;
