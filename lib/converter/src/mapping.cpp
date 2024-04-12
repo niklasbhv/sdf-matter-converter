6febb8ad58a276f1e60c4c05ae765b6158715bd4
Niklas Meyer <nik_mey@uni-bremen.de>
2024-04-12 13:50:51 +0200

    Removed broken code from mapping

--- lib/converter/src/mapping.cpp
+++ lib/converter/src/mapping.cpp
@@ -48,143 +48,36 @@ int resolve_mapping(const std::string& reference, const std::string& entry, T& r
 //! sdfEvent -> Matter event
 int map_sdf_event(sdfEventType& sdfEvent, eventType& event)
 {
-    event.name = sdfEvent.label;
-    event.description = sdfEvent.description;
-    // access
-    // field
-    // code
-    // side
-    // priority
     return 0;
 }
 
 //! sdfAction -> Matter command
 int map_sdf_action(sdfActionType& sdfAction, commandType& command)
 {
-    command.name = sdfAction.label;
-    command.description = sdfAction.description;
-    //TODO: This needs further mapping as these need to be split up
     return 0;
 }
 
 //! sdfProperty -> Matter attribute
 int map_sdf_property(sdfPropertyType& sdfProperty, attributeType& attribute, pugi::xml_node& sdf_property_node)
 {
-    auto current_action_node = sdf_property_node.append_child(sdfProperty.label.c_str());
-    attribute.name = sdfProperty.label;
-    attribute.description = sdfProperty.description;
-    // access
-    resolve_mapping(current_action_node.path().substr(1), "code", attribute.code);
-    attribute.default_ = sdfProperty.default_;
-    resolve_mapping(current_action_node.path().substr(1), "define", attribute.define);
-    // introducedIn
-    // length
-    // manufacturerCode
-    //TODO: Does this match?
-    if (sdfProperty.maximum.has_value())
-        attribute.max = sdfProperty.maximum.value();
-    if (sdfProperty.minimum.has_value())
-        attribute.min = sdfProperty.minimum.value();
-    // reportMaxInterval
-    // reportMinInterval
-    // reportableChange
-    // optional
-    resolve_mapping(current_action_node.path().substr(1), "side", attribute.side);
-    attribute.type = sdfProperty.type; //TODO: Definitely needs mapping
-    // reportable
-    // array
-    attribute.isNullable = sdfProperty.nullable;
-    //TODO: This will default to false, check documentation for the actual default
-    if (sdfProperty.readable.has_value())
-        attribute.readable = sdfProperty.readable.value();
-    if (sdfProperty.writable.has_value())
-        attribute.writable = sdfProperty.writable.value();
     return 0;
 }
 
 //! sdfObject -> Matter cluster
 int map_sdf_object(sdfObjectType& sdfObject, clusterType& cluster, pugi::xml_node& sdf_object_node)
 {
-    auto current_object_node = sdf_object_node.append_child(sdfObject.label.c_str());
-    // Common qualities
-    cluster.name = sdfObject.label;
-    cluster.description = sdfObject.description;
-    resolve_mapping(current_object_node.path().substr(1), "domain", cluster.domain);
-    resolve_mapping(current_object_node.path().substr(1), "code", cluster.code);
-    resolve_mapping(current_object_node.path().substr(1), "define", cluster.define);
-    // server
-    // client
-    // generateCmdHandlers
-    // tag
-    // globalAttribute
-    //save_to_mapping("introducedIn", cluster.introducedIn);
-    //save_to_mapping("manufacturerCode", cluster.manufacturerCode);
-    // singleton
-
-    // Iterate through sdfProperties
-    auto sdf_property_node = current_object_node.append_child("sdfProperty");
-    for (auto sdfProperty : sdfObject.sdfProperty){
-        attributeType attribute;
-        map_sdf_property(sdfProperty.second, attribute, sdf_property_node);
-        cluster.attributes.push_back(attribute);
-    }
-
-    // Iterate through sdfActions
-    for (auto sdfAction : sdfObject.sdfAction){
-        commandType command;
-        map_sdf_action(sdfAction.second, command);
-        cluster.commands.push_back(command);
-    }
-
-    // Iterate through sdfEvents
-    for (auto sdfEvent : sdfObject.sdfEvent){
-        eventType event;
-        map_sdf_event(sdfEvent.second, event);
-        cluster.events.push_back(event);
-    }
     return 0;
 }
 
 //! sdfThing -> Matter device
 int map_sdf_thing(sdfThingType& sdfThing, deviceType& device, pugi::xml_node& sdf_thing_node)
 {
-    auto current_thing_node = sdf_thing_node.append_child(sdfThing.label.c_str());
-    // Common qualities
-    device.name = sdfThing.label;
-    resolve_mapping(current_thing_node.path().substr(1), "domain", device.domain);
-    resolve_mapping(current_thing_node.path().substr(1), "typeName", device.typeName);
-    resolve_mapping(current_thing_node.path().substr(1), "profileId", device.profileId);
-    resolve_mapping(current_thing_node.path().substr(1), "deviceId", device.deviceId);
-    // channels
-
-    auto sdf_object_node = current_thing_node.append_child("sdfObject");
-    for (auto sdfObject : sdfThing.sdfObject){
-        clusterType cluster;
-        map_sdf_object(sdfObject.second, cluster, sdf_object_node);
-        device.clusters.push_back(cluster);
-    }
-    //TODO: How do we handle Properties, Actions and Events of a sdfThing?
     return 0;
 }
 
 //! SDF Model + SDF Mapping -> Matter device
 int map_sdf_to_matter(sdfModelType& sdfModel, sdfMappingType& sdfMappingType, deviceType& device)
 {
-    // Make the mapping a global variable
-    if (!sdfMappingType.map.empty())
-        reference_map = sdfMappingType.map;
-
-    // Initialize a reference tree used to resolve json references
-    pugi::xml_document reference_tree;
-    auto sdf_thing_node = reference_tree.append_child("#").append_child("sdfThing");
-
-
-    if (sdfModel.sdfThing.has_value()){
-        map_sdf_thing(sdfModel.sdfThing.value(), device, sdf_thing_node);
-    } else if (sdfModel.sdfObject.has_value()){
-        // TODO: If no sdfThings are present, a new device with a single cluster has to be created
-    }
-
     return 0;
 }
 
@@ -287,17 +180,6 @@ int map_matter_access(accessType& access, dataQualityType& dataQuality)
 //! Matter Event -> sdfEvent
 int map_matter_event(eventType& event, sdfEventType& sdfEvent, pugi::xml_node& sdf_event_node)
 {
-    // Append the event node to the tree
-    auto event_node = sdf_event_node.append_child(event.name.c_str());
-    event_node.append_attribute("code").set_value(event.code.c_str());
-    event_node.append_attribute("side").set_value(event.side.c_str());
-    sdfEvent.label = event.name;
-    sdfEvent.description = event.description;
-    // access
-    // priority
-    for (eventFieldType& eventField : event.field){
-        sdfEvent.sdfOutputData.sdfChoice.insert({});
-    }
     return 0;
 }
 
@@ -305,71 +187,6 @@ int map_matter_event(eventType& event, sdfEventType& sdfEvent, pugi::xml_node& s
 //! Used if a client and a server command need to be processed
 int map_matter_command(commandType& client_command, commandType& server_command, sdfActionType& sdfAction, pugi::xml_node& sdf_action_node)
 {
-    // Append the command node to the tree
-    auto command_node = sdf_action_node.append_child(client_command.name.c_str());
-    command_node.append_attribute("code").set_value(client_command.code.c_str());
-    if (!client_command.introducedIn.empty())
-        command_node.append_attribute("introducedIn").set_value(client_command.introducedIn.c_str());
-    if (!client_command.manufacturerCode.empty())
-        command_node.append_attribute("manufacturerCode").set_value(client_command.manufacturerCode.c_str());
-
-    sdfAction.label = client_command.name;
-    sdfAction.description = client_command.description;
-
-    //sdfAction.sdfData.insert()
-    // access
-
-    // Map client command arguments
-    auto sdf_input_data_node = command_node.append_child("sdfInputData");
-    for (argType &arg: client_command.arg){
-        dataQualityType dataQualities;
-        auto arg_node = sdf_input_data_node.append_child(arg.name.c_str());
-        // Common qualities
-        dataQualities.label = arg.name;
-        dataQualities.description = arg.description;
-
-        dataQualities.default_ = arg.default_;
-        dataQualities.nullable = arg.isNullable;
-        map_matter_type(arg.type.name, dataQualities);
-        // arraylength
-        // array
-        if (!arg.introducedIn.empty())
-            arg_node.append_attribute("introducedIn").set_value(arg.introducedIn.c_str());
-        if (!arg.removedIn.empty())
-            arg_node.append_attribute("removedIn").set_value(arg.removedIn.c_str());
-        // length
-        // presentIf
-        // optional
-        // fieldIf
-        // countArg
-        sdfAction.sdfInputData.sdfChoice.insert({arg.name, dataQualities});
-    }
-
-    // Map server command arguments
-    auto sdf_output_data_node = command_node.append_child("sdfOutputData");
-    for (argType &arg: server_command.arg) {
-        dataQualityType dataQualities;
-        auto arg_node = sdf_output_data_node.append_child(arg.name.c_str());
-        // Common qualities
-        dataQualities.label = arg.name;
-        dataQualities.description = arg.description;
-
-        dataQualities.default_ = arg.default_;
-        dataQualities.nullable = arg.isNullable;
-        map_matter_type(arg.type.name, dataQualities);
-        // arraylength
-        // array
-        if (!arg.introducedIn.empty())
-            arg_node.append_attribute("introducedIn").set_value(arg.introducedIn.c_str());
-        if (!arg.removedIn.empty())
-            arg_node.append_attribute("removedIn").set_value(arg.removedIn.c_str());
-        // length
-        // presentIf
-        // optional
-        // fieldIf
-        // countArg
-        sdfAction.sdfOutputData.sdfChoice.insert({arg.name, dataQualities});
-    }
     return 0;
 }
 
@@ -377,188 +194,24 @@ int map_matter_command(commandType& client_command, commandType& server_command,
 //! Used if only a client command needs to be processed
 int map_matter_command(commandType& client_command, sdfActionType& sdfAction, pugi::xml_node& sdf_action_node)
 {
-    // Append the command node to the tree
-    auto command_node = sdf_action_node.append_child(client_command.name.c_str());
-    command_node.append_attribute("code").set_value(client_command.code.c_str());
-    if (!client_command.introducedIn.empty())
-        command_node.append_attribute("introducedIn").set_value(client_command.introducedIn.c_str());
-    if (!client_command.manufacturerCode.empty())
-        command_node.append_attribute("manufacturerCode").set_value(client_command.manufacturerCode.c_str());
-
-    sdfAction.label = client_command.name;
-    sdfAction.description = client_command.description;
-
-    //sdfAction.sdfData.insert()
-    // access
-
-    // Map client command arguments
-    auto sdf_input_data_node = command_node.append_child("sdfInputData");
-    for (argType &arg: client_command.arg){
-        dataQualityType dataQualities;
-        auto arg_node = sdf_input_data_node.append_child(arg.name.c_str());
-        // Common qualities
-        dataQualities.label = arg.name;
-        dataQualities.description = arg.description;
-
-        dataQualities.default_ = arg.default_;
-        dataQualities.nullable = arg.isNullable;
-        map_matter_type(arg.type.name, dataQualities);
-        // arraylength
-        // array
-        if (!arg.introducedIn.empty())
-            arg_node.append_attribute("introducedIn").set_value(arg.introducedIn.c_str());
-        if (!arg.removedIn.empty())
-            arg_node.append_attribute("removedIn").set_value(arg.removedIn.c_str());
-        // length
-        // presentIf
-        // optional
-        // fieldIf
-        // countArg
-        sdfAction.sdfInputData.sdfChoice.insert({arg.name, dataQualities});
-    }
     return 0;
 }
 
 //! Matter Attribute -> sdfProperty
 int map_matter_attribute(attributeType& attribute, sdfPropertyType& sdfProperty, pugi::xml_node& sdf_property_node)
 {
-    // Append the attribute node to the tree
-    auto attribute_node = sdf_property_node.append_child(attribute.name.c_str());
-    attribute_node.append_attribute("code").set_value(attribute.code.c_str());
-    attribute_node.append_attribute("define").set_value(attribute.define.c_str());
-    if (!attribute.introducedIn.empty())
-        attribute_node.append_attribute("introducedIn").set_value(attribute.introducedIn.c_str());
-    if (!attribute.manufacturerCode.empty())
-        attribute_node.append_attribute("manufacturerCode").set_value(attribute.manufacturerCode.c_str());
-    attribute_node.append_attribute("side").set_value(attribute.side.c_str());
-
-    sdfProperty.label = attribute.name;
-    sdfProperty.description = attribute.description;
-    if (!attribute.optional){
-        // TODO: Where do sdfRequired definitions of this kind have to go?
-        sdfProperty.sdfRequired = {attribute_node.path().substr(1)};
-    }
-
-    map_matter_type(attribute.type, sdfProperty);
-    sdfProperty.default_ = attribute.default_;
-    // TODO: Do these match?
-    if (attribute.min.has_value())
-        sdfProperty.minLength = attribute.min.value();
-    if (attribute.max.has_value())
-        sdfProperty.maxLength = attribute.max.value();
-    if (attribute.isNullable.has_value())
-        sdfProperty.nullable = attribute.isNullable.value();
-    // access
-    // length
-    // reportMaxInterval
-    // reportMinInterval
-    // reportableChange
-    // array
-    if (attribute.readable.has_value())
-        sdfProperty.readable = attribute.readable.value();
-    if (attribute.writable.has_value())
-        sdfProperty.writable = attribute.writable.value();
-    //sdfProperty.observable = attribute.reportable; //TODO: Does this match
     return 0;
 }
 
 //! Matter Cluster -> sdfObject
 int map_matter_cluster(clusterType& cluster, sdfObjectType& sdfObject, pugi::xml_node& sdf_object_node)
 {
-    // Append the name of the cluster to the tree
-    // Also append sdfProperty, sdfAction and sdfEvent to the tree
-    auto cluster_node = sdf_object_node.append_child(cluster.name.c_str());
-    cluster_node.append_attribute("domain").set_value(cluster.domain.c_str());
-    cluster_node.append_attribute("code").set_value(cluster.code.c_str());
-    cluster_node.append_attribute("define").set_value(cluster.define.c_str());
-    cluster_node.append_attribute("client").set_value(cluster.client);
-    cluster_node.append_attribute("server").set_value(cluster.server);
-    if (!cluster.introducedIn.empty())
-        cluster_node.append_attribute("introducedIn").set_value(cluster.introducedIn.c_str());
-    if (!cluster.manufacturerCode.empty())
-        cluster_node.append_attribute("manufacturerCode").set_value(cluster.manufacturerCode.c_str());
-    cluster_node.append_child("sdfProperty");
-    cluster_node.append_child("sdfAction");
-    cluster_node.append_child("sdfEvent");
-
-    // Map individual cluster fields
-    sdfObject.label = cluster.name;
-    sdfObject.description = cluster.description;
-
-    // Iterate through the attributes and map them
-    auto attribute_node = cluster_node.child("sdfProperty");
-    for (attributeType& attribute : cluster.attributes){
-        sdfPropertyType sdfProperty;
-        map_matter_attribute(attribute, sdfProperty, attribute_node);
-        sdfObject.sdfProperty.insert({attribute.name, sdfProperty});
-    }
-
-    // TODO: Check if standalone server commands are possible
-    // For every Matter client command there might be a corresponding server command that needs to be merged
-    // Iterate through the commands and map them
-    for (commandType& command : cluster.commands){
-        auto command_node = cluster_node.child("sdfAction");
-        bool command_mapped = false;
-        if (command.source == "client"){
-            for (commandType& server_command : cluster.commands){
-                if (command.code == server_command.code and server_command.source == "server"){
-                    sdfActionType sdfAction;
-                    map_matter_command(command, server_command, sdfAction, command_node);
-                    sdfObject.sdfAction.insert({command.name, sdfAction});
-                    command_mapped = true;
-                }
-            }
-            if(!command_mapped){
-                sdfActionType sdfAction;
-                map_matter_command(command, sdfAction, command_node);
-                sdfObject.sdfAction.insert({command.name, sdfAction});
-            }
-        }
-    }
-
-    // Iterate through the events and map them
-    auto event_node = cluster_node.child("sdfEvent");
-    for (eventType& event : cluster.events){
-        sdfEventType sdfEvent;
-        map_matter_event(event, sdfEvent, event_node);
-        sdfObject.sdfEvent.insert({event.name, sdfEvent});
-    }
-
     return 0;
 }
 
 //! Matter Device -> SDF-Model
 int map_matter_device(deviceType& device, sdfModelType& sdfModel, pugi::xml_node& sdf_thing_node)
 {
-    // Append a new sdfObject node to the tree
-    sdf_thing_node.append_child(device.name.c_str()).append_child("sdfObject");
-    auto device_node = sdf_thing_node.child(device.name.c_str());
-    device_node.append_attribute("domain").set_value(device.domain.c_str());
-    device_node.append_attribute("typeName").set_value(device.typeName.c_str());
-    device_node.append_attribute("profileId").set_value(device.profileId.c_str());
-    device_node.append_attribute("deviceId").set_value(device.deviceId.c_str());
-    auto sdf_object_node = device_node.child("sdfObject");
-    // channels
-
-    // Map the information block
-    sdfModel.infoBlock.title = device.name;
-
-    // Map the namespace block
-    sdfModel.namespaceBlock.namespaces.insert({"zcl", ""});
-    sdfModel.namespaceBlock.defaultNamespace = "zcl";
-
-    // Map the definition block
-    sdfThingType sdfThing;
-    sdfThing.label = device.name;
-
-    // Iterate through cluster definitions for the device
-    for (auto cluster : device.clusters){
-        sdfObjectType sdfObject;
-        map_matter_cluster(cluster, sdfObject, sdf_object_node);
-        sdfThing.sdfObject.insert({cluster.name, sdfObject});
-    }
-    sdfModel.sdfThing = sdfThing;
-
     return 0;
 }
 
