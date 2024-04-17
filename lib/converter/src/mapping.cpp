9e847c980cc6ab59673a40b08f0cb8048eff1638
Niklas Meyer <nik_mey@uni-bremen.de>
2024-04-17 09:34:23 +0200

    Implemented types and integer boundaries for matter type conversion

--- lib/converter/src/mapping.cpp
+++ lib/converter/src/mapping.cpp
@@ -123,31 +123,75 @@ int map_matter_type(std::string& matter_type, dataQualityType& dataQuality)
     // TODO: Custom types should be handled here
     // TODO: Maybe also set the default value here?
     if (matter_type == "bool") {
-
+        dataQuality.type = "boolean";
     }
     if (matter_type == "single") {
-
+        dataQuality.type = "number";
     }
     if (matter_type == "double") {
-
+        dataQuality.type = "number";
     }
     if (matter_type == "octstr") {
-
+        dataQuality.type = "string";
     }
     if (matter_type == "list") {
-
+        dataQuality.type = "array";
     }
     if (matter_type == "struct") {
-
+        dataQuality.type = "object";
     }
     if (matter_type.substr(0, 3) == "map") {
-
+        dataQuality.type = "string";
     }
     if (matter_type.substr(0, 3) == "int") {
-
+        // TODO: These boundaries change if the corresponding value is nullable
+        dataQuality.type = "integer";
+        dataQuality.minimum = 0;
+        if (matter_type.substr(3) == "8") {
+            dataQuality.maximum = 2^8-1;
+        } else if (matter_type.substr(3) == "16") {
+            dataQuality.maximum = 2^16-1;
+        } else if (matter_type.substr(3) == "24") {
+            dataQuality.maximum = 2^24-1;
+        } else if (matter_type.substr(3) == "32") {
+            dataQuality.maximum = 2^32-1;
+        } else if (matter_type.substr(3) == "40") {
+            dataQuality.maximum = 2^40-1;
+        } else if (matter_type.substr(3) == "48") {
+            dataQuality.maximum = 2^48-1;
+        } else if (matter_type.substr(3) == "56") {
+            dataQuality.maximum = 2^56-1;
+        } else if (matter_type.substr(3) == "64") {
+            dataQuality.maximum = 2^64-1;
+        }
     }
     if (matter_type.substr(0, 4) == "uint") {
-
+        dataQuality.type = "integer";
+        if (matter_type.substr(4) == "8") {
+            dataQuality.minimum = -2^7;
+            dataQuality.maximum = 2^7-1;
+        } else if (matter_type.substr(4) == "16") {
+            dataQuality.minimum = -2^15;
+            dataQuality.maximum = 2^15-1;
+        } else if (matter_type.substr(4) == "24") {
+            dataQuality.minimum = -2^23;
+            dataQuality.maximum = 2^23-1;
+        } else if (matter_type.substr(4) == "32") {
+            dataQuality.minimum = -2^31;
+            dataQuality.maximum = 2^31-1;
+        } else if (matter_type.substr(4) == "40") {
+            dataQuality.minimum = -2^39;
+            dataQuality.maximum = 2^39-1;
+        } else if (matter_type.substr(4) == "48") {
+            dataQuality.minimum = -2^47;
+            dataQuality.maximum = 2^47-1;
+        } else if (matter_type.substr(4) == "56") {
+            dataQuality.minimum = -2^55;
+            dataQuality.maximum = 2^55-1;
+        } else if (matter_type.substr(4) == "64") {
+            dataQuality.minimum = -2^63;
+            dataQuality.maximum = 2^63-1;
+        }
     }
 
     // If the type is not a known standard type
@@ -197,6 +241,15 @@ int map_matter_cluster(const clusterType& cluster, sdfObjectType& sdfObject, pug
 //! Matter Device -> SDF-Model
 int map_matter_device(const deviceType& device, sdfModelType& sdfModel, pugi::xml_node& sdf_thing_node)
 {
+    // Append a new sdfObject node to the tree
+    sdf_thing_node.append_child(device.name.c_str()).append_child("sdfObject");
+    auto device_node = sdf_thing_node.child(device.name.c_str());
+    device_node.append_attribute("id").set_value(device.id);
+    // conformance
+    // access
+    sdfModel.infoBlock.title = device.name;
+    device_node.append_attribute("revision").set_value(device.revision);
+    // TODO: We need to be able to create a JSON object for more complex structures like revisionHistory
     return 0;
 }
 
